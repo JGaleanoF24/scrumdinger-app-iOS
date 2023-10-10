dbb071db592942540e083b7112c7e9c163b4658f
Josimar Galeano <ing.galeano24@gmail.com>
2023-10-10 18:10:22 -0500

    Display data in a list

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -24,10 +24,11 @@ enum Theme: String {
     case tan
     case teal
     case yellow
+    case DuskTabBar
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow, .DuskTabBar: return .black
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
